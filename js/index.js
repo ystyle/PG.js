217bad5a8cea614116b81f76a5b9aa11fded033b
YSTYLE <lxy5266@live.com>
2015-08-22 15:00:04 +0800

    完成切换成Angular.js

--- js/index.js
+++ js/index.js
@@ -12,133 +12,152 @@ $(function () {
         $(this).tab('show');
     });
 
-    initUI();
     //默认显示第一个面板
     $(".tabs-nwjs:first").show();
 
-    // 打开文件
-    $("#load").click(function (e) {
-        $("#fileDialog").trigger("click");
-    });
-
-    // 重新读取
-    $("#refresh").click(function (e) {
-        if (globalSetting.packagePath) {
-            loadJSONFile();
+    // 文件选择框更改事件
+    $("#fileDialog").change(function () {
+        if (this.value) {
+            window.localStorage.setItem('LastReadPath',this.value);
+            var json = fs.readFileSync(this.value);
+            window.localStorage.setItem('packages', json);
+            $("#refresh").trigger("click");
         }
     });
-
-    // 保存文件
-    $("#save").click(function (e) {
-        if (globalSetting.packagePath) {
-            saveJSONFile();
-        } else {
-            $("#fileSaveDialog").trigger("click");
+    //文件保存框更改事件
+    $("#fileSaveDialog").change(function () {
+        if (this.value) {
+            saveJson(this.value);
         }
     });
 });
 
 /**
- * 加载json文件
+ * 保存JSON
+ * @param path
  */
-function loadJSONFile() {
-    var JsonObj = JSON.parse(fs.readFileSync(globalSetting.packagePath));
-    globalSetting.json = JsonObj;
-    $("#form-basic").setform(JsonObj);// 基本信息
-    $("#form-window").setform(JsonObj.window);// 窗口设置
-    $("#form-advanced").setform(JsonObj);// 高级设置
-
-    $("#form-maintainers").setform(JsonObj.maintainers); //软件维护者信息
-    if (JsonObj.keywords) {
-        $("#form-keywords").setform({keywords: JsonObj.keywords.join(",")}); // 关键词
-    }
-    $("#form-bugs").setform({bugs: JsonObj.bugs}); // bug提交地址
-    $("#form-contributors").setform(JsonObj.contributors); // 贡献者信息
-    $("#form-licenses").setform(JsonObj.licenses); // licenses
-    $("#form-repositories").setform(JsonObj.repositories); // 仓库地址
+var saveJson = function (path) {
+    window.localStorage.setItem('LastWritePath', path);
+    var json = window.localStorage.getItem('packages');
+    fs.writeFileSync(path, json);
 }
+
+// 初始化 angular
+var app = angular.module('pg', []).config(['$controllerProvider', function ($controllerProvider) {
+    $controllerProvider.allowGlobals();
+}]);
+
 /**
- * 保存json文件
+ * 定义Angular的controller
  */
-function saveJSONFile() {
-    var basic = $("#form-basic").serializeJson();// 基本信息
-    var windowobj = $("#form-window").serializeJson();// 窗口设置
-    var advanced = $("#form-advanced").serializeJson();// 高级设置
+app.controller('packageGeneratorCtrl', function ($scope,Packages,$timeout) {
 
+    // pageage.json 内容
+    $scope.package = Packages.all();
 
-    var maintainers = $("#form-maintainers").serializeJson(); //软件维护者信息
-    var keywordsstr = $("#form-keywords").serializeJson(); // 关键词
-    var bugs = $("#form-bugs").serializeJson(); // bug提交地址
-    var contributors = $("#form-contributors").serializeJson(); // 贡献者信息
-    var licenses = $("#form-licenses").serializeJson(); // licenses
-    var repositories = $("#form-repositories").serializeJson(); // 仓库地址
+    // 初始化界面时弹出的信息
+    $scope.temppathMsg = "打开文件:"+Packages.getLastReadPath()+"\n"+"保存路径:"+Packages.getLastWritePath();
 
-    // 合并对象
-    var newmerge = {
-        maintainers: maintainers,
-        keywords: function () {
-            if (keywordsstr) {
-                return keywordsstr.split(',');
-            } else {
-                return [];
+    /**
+     * 清除本地缓存
+     */
+    $scope.clearStorage = function () {
+        window.localStorage.clear();
+        $scope.refresh();
+    }
+
+    /**
+     * 关闭模式化窗口
+     */
+    $scope.cloaseModal = function () {
+        $('#myModal').modal('hide');
+    }
+
+    /**
+     * 打开文件选择框
+     */
+    $scope.openFileDialog = function () {
+        $("#fileDialog").trigger("click");
+    }
+
+    /**
+     * 保存文件
+     * @param package
+     */
+    $scope.savePackage = function (package) {
+        Packages.save(package);
+        var path = Packages.getLastWritePath();
+        if(!path){
+            $("#fileSaveDialog").trigger("click");
+        }else{
+            saveJson(path);
+        }
+    }
+
+    /**
+     * 刷新界面数据
+     */
+    $scope.refresh = function () {
+        $scope.package = Packages.all();
+    }
+
+    /**
+     * 初始化界面检测上一次未关闭文件
+     */
+    $timeout(function () {
+        if (Packages.getLastReadPath() || Packages.getLastWritePath()) {
+            $('#myModal').modal('show');
+        }
+    });
+});
+
+app.factory('Packages', function () {
+    return {
+        /**
+         * 查询package.json
+         * @returns {*}
+         */
+        all: function () {
+            var packageString = window.localStorage.getItem('packages');
+            if (packageString) {
+                return angular.fromJson(packageString);
             }
+            return {};
         },
-        bugs: function () {
-            if (bugs) {
-                return bugs;
-            } else {
-                return "";
-            }
+        /**
+         * 保存package.json
+         * @param packages
+         */
+        save: function (packages) {
+            window.localStorage.setItem('packages', angular.toJson(packages,true));
         },
-        contributors: function () {
-            if (contributors) {
-                return contributors;
-            } else {
-                return {};
-            }
+        /**
+         * 取得上一次读取文件的路径
+         * @returns {string}
+         */
+        getLastReadPath: function () {
+            return window.localStorage.getItem('LastReadPath') || '';
         },
-        licenses: function () {
-            if (licenses) {
-                return licenses;
-            } else {
-                return {};
-            }
+        /**
+         * 设置当前使用的文件路径
+         * @param path
+         */
+        setLastReadPath: function (path) {
+            return window.localStorage.setItem('LastReadPath',path);
         },
-        repositories: function () {
-            if (repositories) {
-                return repositories;
-            } else {
-                return {};
-            }
+        /**
+         * 取得上一次保存文件的路径
+         * @returns {string}
+         */
+        getLastWritePath: function () {
+            return window.localStorage.getItem('LastWritePath') || '';
+        },
+        /**
+         * 设置当前保存文件的路径
+         * @param path
+         */
+        setLastWritePath: function (path) {
+            window.localStorage.setItem('LastWritePath', path);
         }
     };
-
-    var json = $.extend(basic, advanced, newmerge);
-
-    if (window) {
-        json.window = windowobj;
-    }
-    globalSetting.json = json;
-    fs.writeFileSync(globalSetting.packagePath, JSON.stringify(globalSetting.json));
-}
-
-/**
- * 初始化界面
- */
-function initUI() {
-    $("#fileDialog").change(function () {
-        if (this.value) {
-            globalSetting.packagePath = this.value;
-            loadJSONFile();
-        }
-    });
-    $("#fileSaveDialog").change(function () {
-        if (this.value) {
-            globalSetting.packagePath = this.value;
-            saveJSONFile();
-        }
-    });
-}
-
-// 文件保存路径
-var globalSetting = {};
+});
